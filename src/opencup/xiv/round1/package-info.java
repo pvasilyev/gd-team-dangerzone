64a1494ecc698f3bd6c02864f882b4741cb30c09
Pavel Vasilyev <p_vasiliev@mail.ru>
2013-10-14 13:53:40 +0400

    package-info files are added with problem sets and schedule links

---
+++ src/opencup/xiv/round1/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * This package contains <i>GD-team</i> solutions (up-solvings) for the first round of Open Cup 2013 autumn-session.
+ * <p/>
+ * The whole schedule of 2013 autumn-session is here:
+ * <a href="http://opencup.ru/index.cgi?data=oce/schedule&menu=index&head=index">Grand Prix of SPb</a>.
+ * <p/>
+ *
+ * To see actual status for the up-solvings please follow the google-document (access required):
+ * <a href="https://docs.google.com/spreadsheet/ccc?key=0AmFL-geGyo4qdFRqZnFmQkRkVm52WWRubFVJZFBuM1E&usp=sharing#gid=0">
+ *     opencup up-solvings
+ * </a>
+ * <p/>
+ *
+ * Problem set can be found here:
+ * <a href="https://docs.google.com/file/d/0Bw-ISPWOGPVJbTJNdGVfMmhzSVE/edit?usp=drive_web">opencup20131006.pdf</a>
+ *
+ * @since 6 Oct 2013
+ */
+
+package opencup.xiv.round1;
